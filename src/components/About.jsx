a695797385fa59570b15f89d15b06a139847ec6c
Alejandro Lopez Ramirez <alejozepol@gmail.com>
2020-04-16 17:52:23 -0500

    diseño header con estilos e iconos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import iconPhoto from '../assets/IconPhone.svg';
+import iconEmail from '../assets/IconEmail.svg';
+import iconLocation from '../assets/IconLocation.svg';
+import iconWeb from '../assets/IconWeb.svg';
 
 const About = ({title, phone, email, website, address}) => {
   return(
     <>
     <h2 className='About-title'>{title}</h2>
     <div className='About-items'>
-      <p className='About-item'>{phone}</p>
-      <p className='About-item'>{email}</p>
-      <p className='About-item'>{website}</p>
-      <p className='About-item'>{address}</p>
+      <div className='About-item'>
+        <img src={iconPhoto} alt='phone icon' />
+        <p>{phone}</p>
+      </div>
+      <div className='About-item'>
+        <img src={iconEmail} alt='email icon' />
+        <p>{email}</p>
+      </div>
+      <div className='About-item'>
+        <img src={iconWeb} alt='website icon' />
+        <p>{website}</p>
+      </div>
+      <div className='About-item'>
+        <img src={iconLocation} alt='address icon' />
+        <p>{address}</p>
+      </div>
     </div>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import cover from '../assets/cover.svg';
+import marco from '../assets/marco.svg';
 
 const Header = ({ children, title, img }) => {
   return (
     <section className='Header'>
-      <img src={img} alt={title}/>
-      <h1 className='Header-title'>{title}</h1>
+        <img className='Header-banner' src={cover} alt='Banner' />
+        <div className='Header-avatar'>
+          <img className='Header-avatar-photo' src={img} alt={title} />
+          <img className='Header-avatar-marco' src={marco} alt='marco' />
+        </div>
+        <h1 className='Header-title'>{title}</h1>
       {children}
     </section>
   )
