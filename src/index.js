0f70aff48f7888613085299b92ed1b9252d942b4
David Mosquera <mosqueradavid@outlook.es>
2020-04-27 16:42:58 -0500

    fix context folder & getData.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ThemeProvider} from '../src/context/ThemeContext';
+import { ThemeProvider } from './context/ThemeContext';
 import App from './containers/App';
 
 ReactDOM.render(
   <ThemeProvider>
     <App />
   </ThemeProvider>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,12 +1,12 @@
-const API = 'http:localhost:3000/data'
 const getData = async (API) => {
   try {
-    const data = await fetch(API)
-    const info = await data.json()
-    return info
-  } catch(error) {
-    console.error('Error fetching data', error)
+    const data = await fetch(API);
+    const info = await data.json();
+    return info;
+  } catch (error) {
+    console.error('Error fetching data', error);
   }
-}
+  return data;
+};
 
 export default getData;
