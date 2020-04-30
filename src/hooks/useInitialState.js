baa410a506db90b61ef0dc04fb39edc55808e138
Erickowski <daniel_perez_03081995@hotmail.com>
2020-04-30 11:07:48 -0500

    Hooks initial state

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,19 +1,19 @@
 import {
-    useState,
-    useEffect
+  useState,
+  useEffect,
 } from 'react';
 
 const useInitialState = (API) => {
-    const [info, setInfo] = useState([]);
-    useEffect(() => {
-        fetch(API)
-            .then(response => response.json())
-            .then(data => setInfo(data))
-            .catch((error) => {
-                return error.message;
-            })
-    }, []);
-    return info;
+  const [info, setInfo] = useState([]);
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => setInfo(data))
+      .catch((error) => {
+        return error.message;
+      });
+  }, []);
+  return info;
 };
 
 export default useInitialState;
