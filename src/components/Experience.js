0bb0098e786a14d348dde78eba8e8ee2be2ba988
Razielini <razielco@gmail.com>
2020-06-01 08:35:10 -0600

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,18 +2,13 @@ import React from 'react';
 import '../styles/components/Experience.styl';
 
 const Experience = ({ info = [] }) => {
-  const itemKey = () => {
-    let count = 0;
-    return `interest-key${count++}`;
-  };
-
   return (
     <div className='Experience'>
       <h3 className='Experience-title'>Experience Title</h3>
       <div>
         {
           info.map((item, index) => (
-            <p className='Experience-item' key={itemKey}>
+            <p className='Experience-item' key={item.id}>
               <b>{ item.company }</b>
               <br />
               { item.startDate}
