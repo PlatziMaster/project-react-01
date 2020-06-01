303a819ec20c2d1c39239eea4ba31f27dd74e1d9
Razielini <razielco@gmail.com>
2020-06-01 08:35:48 -0600

    Update Languages.js

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -3,18 +3,13 @@ import '../styles/components/Languages.styl';
 
 const Languages = ({ info = [] }) => {
   console.log('info :: ', info);
-
-  const itemKey = () => {
-    let count = 0;
-    return `languages-key${count++}`;
-  };
   return (
     <div className='Languages'>
       <h3 className='Languages-title'>Languages Title</h3>
       <div>
         {
           info.map((lang, index) => (
-            <p className='Languages-item' key={itemKey}>
+            <p className='Languages-item' key={lang.id}>
               { lang.name }
               {' '}
               (
