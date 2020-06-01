8c7f7a92ac49b0f9b142a7869795f52e6b711ee1
Razielini <razielco@gmail.com>
2020-06-01 08:36:04 -0600

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,18 +2,13 @@ import React from 'react';
 import '../styles/components/Skills.styl';
 
 const Skills = ({ info = [] }) => {
-  const itemKey = () => {
-    let count = 0;
-    return `skill-key${count++}`;
-  };
-
   return (
     <div className='Skills'>
       <h3 className='Skills-title'>Skills title</h3>
       <div>
         {
           info.map((item, index) => (
-            <p className='Skills-item' key={itemKey}>
+            <p className='Skills-item' key={item.id}>
               { item.name }
               {' '}
               (
