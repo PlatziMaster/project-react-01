0b8ee441ad4ec0473f236a2459a46586bb8808e4
Gabriel Zarate <gzaratet1@gmail.com>
2020-08-14 12:34:08 -0500

    Agregando la utileria getData para hacer fetch a data.json, reparando bugs

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/components/App.styl';
 import Header from '../components/Header';
 import About from '../components/About';
@@ -8,19 +8,44 @@ import Academic from '../components/Academic';
 import Skills from '../components/Skills';
 import Interest from '../components/Interest';
 import Languages from '../components/Languages';
+import getData from '../utils/getData';
+
+function getState(_URL) {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    getData(_URL)
+      .then(initialState => setState(initialState));
+  }, []);
+
+  return state;
+};
 
 const App = () => {
+  const URL = 'http://localhost:3000/data';
+  const state = getState(URL);
   return (
     <>
-      <Header>
-        <About />
+      <Header title='Currículum Vitae'>
+        <About
+          name={state.name}
+          profession={state.profession}
+          phone={state.phone}
+          email={state.email}
+          website={state.website}
+          address={state.address}
+        />
       </Header>
-      <Profile />
-      <Experience />
-      <Academic />
-      <Skills />
-      <Interest />
-      <Languages />
+      <Profile
+        title='About me'
+        profile={state.Profile}
+        social={state.social}
+      />
+      <Experience title='Experience' experience={state.experience} />
+      <Academic title='Academic' academic={state.Academic} />
+      <Skills title='Skills' skills={state.skills} />
+      <Interest title='Interest' interest={state.interest} />
+      <Languages title='Languages' languages={state.languages} />
     </>
   );
 };
--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -4,7 +4,7 @@ const gravatar = (email) => {
   const base = 'https://gravatar.com/avatar/';
   const formatedEmail = email.trim().toLowerCase();
   const hash = md5(formatedEmail, { encoding: 'binary' });
-  return `${base}${hash}`;
+  return `${base}${hash}?s=200`;
 };
 
 export default gravatar;
