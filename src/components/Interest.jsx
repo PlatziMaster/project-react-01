9c4e69c92185c9a44fd90486d75c30135784fdaf
Pedro Cruz <pcruzmay@yahoo.com>
2020-09-07 11:43:28 -0500

    reto curriculum

---
+++ src/components/Interest.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/components/Interest.styl';
+
+const Interest = ({ interest = ['', '', '', '', ''] }) => {
+    return (
+        <section className='Interest'>
+            <h2 className="Interest-title">Intereses</h2>
+            <section className='Interests'>
+                {interest.map((item, index) => (<p className='Interest-item' key={index} >{item}</p>))
+
+                }
+            </section>
+        </section>
+    )
+}
+
+export default Interest
---
+++ src/utils/getData.js
@@ -0,0 +1,7 @@
+const getData = (API) => {
+    return fetch(API)
+        .then(data => data.json())
+        .then(response => response)
+        .catch(error => error);
+};
+export default getData;
